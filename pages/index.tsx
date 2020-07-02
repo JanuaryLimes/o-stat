a473d94d8e4cf78fa9340c078da01492e3c8225f
kacper.sledz <casper.sledx@gmail.com>
2020-07-02 20:22:42 +0200

    tailwind style, load image, recognize text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,60 @@
-import Link from 'next/link';
 import Layout from '../components/Layout';
+import { createWorker } from 'tesseract.js';
+import { ChangeEvent, useState } from 'react';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About test...2</a>
-      </Link>
-      test
-    </p>
-  </Layout>
-);
+const IndexPage = () => {
+  const [fileSrc, setFileSrc] = useState<string | undefined>(undefined);
+  const [text, setText] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  function click() {
+    if (fileSrc) {
+      setLoading(true);
+      const worker = createWorker();
+
+      (async () => {
+        await worker.load();
+        await worker.loadLanguage('pol');
+        await worker.initialize('pol');
+        const result = await worker.recognize(fileSrc);
+        console.log(result);
+        setText(result.data.text);
+        await worker.terminate();
+        setLoading(false);
+      })();
+    }
+  }
+
+  function onFileSelect(event: ChangeEvent<HTMLInputElement>) {
+    //console.log(event);
+    if (event.target?.files?.[0]) {
+      setFileSrc(URL.createObjectURL(event.target.files[0]));
+    }
+  }
+
+  return (
+    <Layout title="o-stat">
+      <div>
+        <p>obraz</p>
+        <input type="file" onChange={onFileSelect}></input>
+        <img src={fileSrc}></img>
+      </div>
+      <div>
+        <p>wynik</p>
+
+        {loading ? (
+          <div>czekaj...</div>
+        ) : (
+          <>
+            <button className="btn" onClick={click}>
+              tekst
+            </button>
+            <div>{text}</div>
+          </>
+        )}
+      </div>
+    </Layout>
+  );
+};
 
 export default IndexPage;
